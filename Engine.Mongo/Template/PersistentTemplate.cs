15b344ce6e4caba4f5dfd3aad1923198b1a35d31
axmand <815966508@qq.com>
2017-09-30 00:30:11 +0800

    fix

---
+++ Engine.Mongo/Template/PersistentTemplate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Mongo.Template
+{
+    /// <summary>
+    /// 不使用内存数据库
+    /// </summary>
+    public class PersistentTemplate
+    {
+    }
+}
