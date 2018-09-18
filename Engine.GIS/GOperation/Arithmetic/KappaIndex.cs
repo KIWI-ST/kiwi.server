15d1d0af69cd2336390f5aaf24539b97e958fa93
axmand <815966508@qq.com>
2018-09-19 00:19:07 +0800

    fix kappa index calcute (overflow maximum of integer)

--- Engine.GIS/GOperation/Arithmetic/KappaIndex.cs
+++ Engine.GIS/GOperation/Arithmetic/KappaIndex.cs
@@ -16,24 +16,28 @@ namespace Engine.GIS.GOperation.Arithmetic
             //statical label band graph
             IBandStasticTool pBandStaticTool = new GBandStasticTool();
             pBandStaticTool.Visit(truthLayer.BandCollection[0]);
-            Dictionary<int,List<Point>> Memory = pBandStaticTool.StaisticalRawGraph;
+            Dictionary<int,List<Point>> memory = pBandStaticTool.StaisticalRawGraph;
             //key index
-            List<int> Keys = Memory.Keys.ToList();
+            List<int> Keys = memory.Keys.ToList();
             int actionsNumber = Keys.Count;
             int[,] matrix = new int[actionsNumber, actionsNumber];
             IBandCursorTool pBandCursorTool = new GBandCursorTool();
             pBandCursorTool.Visit(predLayer.BandCollection[0]);
+            //
+            pBandStaticTool.Visit(predLayer.BandCollection[0]);
+            var m = pBandStaticTool.StaisticalRawGraph;
+            //
             for (int i = 0; i < actionsNumber; i++)
             {
                 int key = Keys[i];
-                List<Point> points = Memory[key];
+                List<Point> points = memory[key];
                 //计算realKey类分类结果,存入混淆矩阵
                 points.ForEach(p =>
                 {
-                    int classificationType = (int)pBandCursorTool.PickRawValue(p.X, p.Y);
-                    classificationType = Keys.IndexOf(classificationType);
-                    if (classificationType != -1)
-                        matrix[i, classificationType]++;
+                    int rawType = (int)pBandCursorTool.PickRawValue(p.X, p.Y);
+                    int indexType = Keys.IndexOf(rawType);
+                    if (indexType != -1)
+                        matrix[i, indexType]++;
                 });
             }
             // Create a new multi-class Confusion Matrix
@@ -43,11 +47,11 @@ namespace Engine.GIS.GOperation.Arithmetic
             //p0
             double p0 = 0;
             for (int i = 0; i < actionsNumber; i++)
-                p0 += matrix[i, i];
+                p0 += Convert.ToDouble(matrix[i, i]);
             //pc
             double pc = 0;
             for (int i = 0; i < actionsNumber; i++)
-                pc += cm.ColumnTotals[i] * cm.RowTotals[i];
+                pc += Convert.ToDouble(cm.ColumnTotals[i]) * Convert.ToDouble(cm.RowTotals[i]);
             pc = pc / totalNum;
             //
             double kappa = (p0 - pc) / (totalNum - pc);
