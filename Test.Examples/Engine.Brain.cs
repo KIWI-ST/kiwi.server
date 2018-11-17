531de639c6e6abf2141d8fa61f85be2635b0ee60
axmand <815966508@qq.com>
2018-11-17 23:21:20 +0800

    fix Unit Test to x64

---
+++ Test.Examples/Engine.Brain.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.Examples
+{
+    [TestClass]
+    public class BrainTEST
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var s = "";
+        }
+    }
+}
---
+++ Test.Examples/Engine.GIS.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.Examples
+{
+    [TestClass]
+    public class GISTEST
+    {
+        [TestMethod]
+        public void LoadRasterLayer()
+        {
+            string fullFilename = System.IO.Directory.GetCurrentDirectory()+ @"\Datasets\A_Band18.tif";
+            Engine.GIS.GLayer.GRasterLayer.GRasterLayer rasterLayer = new Engine.GIS.GLayer.GRasterLayer.GRasterLayer(fullFilename);
+        }
+    }
+}
