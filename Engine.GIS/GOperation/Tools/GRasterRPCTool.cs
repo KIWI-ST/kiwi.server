3f4fd972a8800568ac09c9530fb342861de4856f
axmand <815966508@qq.com>
2018-12-03 22:36:14 +0800

    Add RPCTool and prepared implement

---
+++ Engine.GIS/GOperation/Tools/GRasterRPCTool.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.GIS.GLayer.GRasterLayer;
+
+namespace Engine.GIS.GOperation.Tools
+{
+    public class GRasterRPCTool : IRasterLayerTool
+    {
+
+        public GRasterRPCTool()
+        {
+            
+        }
+
+        public void Visit(GRasterLayer pLayer)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
--- Engine.Word/Train/SkipGramMethod.cs
+++ Engine.Word/Train/SkipGramMethod.cs
@@ -8,7 +8,8 @@ namespace Engine.Word.Train
 {
     /// <summary>
     /// 使用skip-gram方法对词向量分析
-    /// https://blog.csdn.net/qq1483661204/article/details/78975847
+    /// reference:
+    /// https://github.com/eabdullin/Word2Vec.Net/blob/aa8b48f2fdcb3b66877fd6fba432e69ad811bf62/Word2Vec.Net/Word2Vec.cs#L459
     /// </summary>
     public class SkipGramMethod
     {
