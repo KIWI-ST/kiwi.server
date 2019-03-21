c84950c841fa5df5a9d4570eab7708f976229676
axmand <815966508@qq.com>
2019-03-21 23:34:51 +0800

    fix

---
+++ Engine.Brain/Embed/Embedding.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Engine.Brain.Embed
+{
+    /// <summary>
+    /// https://github.com/axmand/deep-learning-with-csharp-and-cntk/tree/master/DeepLearning/Ch_06_Using_Word_Embeddings
+    /// </summary>
+    public class Embedding
+    {
+    }
+}
